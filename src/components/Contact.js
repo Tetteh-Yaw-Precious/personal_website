8caae44f2e68080247637f5a9799117cc38ca454
Tetteh Yaw Precious <precioustetteh38@gmail.com>
2021-10-22 18:53:17 -0700

    build generated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { device } from "../helpers/Mixins";
 import ContactSocials from "./ContactSocials";
